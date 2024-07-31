18970c49982762bbfe72eea47c2d48c4307ea347
Dibyendu Das <98630356+Dibyendu-dev@users.noreply.github.com>
2024-07-31 16:04:55 +0000

    gallay component is rendered

---
+++ my-app/app/gallary/images.js
@@ -0,0 +1,55 @@
+import photo1 from './photos/pic-1.jpg'
+import photo2 from './photos/pic-2.jpg'
+import photo3 from './photos/pic-3.jpg'
+import photo4 from './photos/pic-4.jpg'
+import photo6 from './photos/pic-6.jpg'
+import photo7 from './photos/pic-7.jpg'
+import photo8 from './photos/pic-8.jpg'
+import photo9 from './photos/pic-9.jpg'
+
+
+const images = [
+    {
+        id: "1",
+        name: "dd - 1",
+        src: photo1,
+    },
+    {
+        id: "2",
+        name: "dd - 2",
+        src: photo2,
+    },
+    {
+        id: "3",
+        name: "dd - 3",
+        src: photo3,
+    },
+    {
+        id: "4",
+        name: "dd - 4",
+        src: photo4,
+    },
+    {
+        id: "6",
+        name: "dd - 6",
+        src: photo6,
+    },
+  
+    {
+        id: "7",
+        name: "dd- 7",
+        src: photo7,
+    },
+    {
+        id: "8",
+        name: "dd - 8",
+        src: photo8,
+    },
+    {
+        id: "9",
+        name: "dd - 9",
+        src: photo9,
+    },
+]
+
+export default images;
